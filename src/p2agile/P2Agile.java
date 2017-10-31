5ea171b447942c574881826d0689cb03a9c30d2c
taruc <taruc@B10>
2017-10-31 11:00:50 +0800

    myfirst branch cimmit

--- src/p2agile/P2Agile.java
+++ src/p2agile/P2Agile.java
@@ -16,6 +16,7 @@ public class P2Agile {
      */
     public static void main(String[] args) {
        System.out.println("Hello world");
+       System.out.println("First branch.!!");
     }
     
 }
